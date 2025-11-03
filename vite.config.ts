a0d989c953dbc577b302f52c8e11a4cf161866f9
research1-alt <research1@omegaseikimobility.com>
2025-11-03 12:29:02 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('xlsx')) {
+              return 'vendor_xlsx';
+            }
+            if (id.includes('chart.js') || id.includes('react-chartjs-2')) {
+              return 'vendor_chartjs';
+            }
+            // All other node_modules go into a general vendor chunk
+            return 'vendor'; 
+          }
+        },
+      },
+    },
+  },
 });
